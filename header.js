bb86ac600601df045fc417c96e405fa27d2773f7
MechanicX86 <mihm098765@gmail.com>
2026-07-31 12:16:14 +0300

    Add files via upload

---
+++ header.js
@@ -0,0 +1,13 @@
+document.write(`
+<header>
+    <h1>MechanicX86</h1>
+    <img src="logo.png" alt="logo">
+</header>
+
+<nav>
+    <a href="index.html">main</a>
+    <a href="projects.html">projects</a>
+    <a href="news.html">news</a>
+    <a href="communication.html">communication</a>
+</nav>
+`);
